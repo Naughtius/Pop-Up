a7fdc762fd6a2965c793177d08c4a0fcae66cec9
Nariman <islamovnariman@gmail.com>
2020-03-11 16:48:11 +0300

    Добавление функционала

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,39 @@
 let links = document.querySelectorAll(".open-popup-link");
-let popupModal = document.querySelectorAll(".popup");
+let popupModals = document.querySelectorAll(".popup");
+let closeBtns = document.querySelectorAll(".popup > i");
+let createBtnClose;
 
-links.forEach((element) => {
+/*Клик по ссылке для открытия модального окна*/
+links.forEach(element => {
     element.addEventListener("click", activePopup);
 });
 
 function activePopup() {
-    if (this.getAttribute("data-src").slice(1)) {
-        console.log('есть!')
-    }
+    document.querySelector("body").classList.add("popup-overflow");
+    document.getElementById(this.getAttribute("data-src").slice(1)).classList.add("popup-active");
 }
+
+/*Добавление кнопки закрытия окна*/
+popupModals.forEach(element => {
+    createBtnClose = document.createElement("i");
+    element.prepend(createBtnClose);
+    createBtnClose.classList.add("fa", "fa-times");
+});
+
+/*Закрытие окна при клике вне области*/
+window.addEventListener("mouseup", e => {
+    popupModals.forEach(element => {
+        if (e.target !== element && !element.contains(e.target) || element.querySelector("i").contains(e.target)) {
+            element.classList.remove("popup-active");
+            document.querySelector("body").classList.remove("popup-overflow");
+        }
+    });
+});
+
+/*Закрытие окна при клике на кнопку*/
+closeBtns.forEach(element => {
+    element.addEventListener("click", () => {
+        element.parentNode.classList.remove("popup-active");
+        document.querySelector("body").classList.remove("popup-overflow");
+    });
+});
